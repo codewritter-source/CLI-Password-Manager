81fbd7b9673ebb4b3720614108c348453a4dd4ed
pachecocruz-source <gj.pachecocruz@gmail.com>
2026-04-15 21:29:03 -0700

    Added a master password function to the password manager.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-
-
+import os
 
 from storage import get_entry, add_entry, add_generated_entry, delete_entry
 from generator import generate
+from master_password import verify_master, generate_master, create_master
 import sys
 
 
-def close_program( ):
+def close_program():
     decision = input("Are you sure you wish to exit? (y/n): ")
     if decision.strip().lower() == "y":
         sys.exit()
 
-def main ():
+def menu():
     #   Menu is getting out of hand. Options: dictionary menu.
     print(
         """"
@@ -19,7 +19,8 @@ Main Menu: \n
     1. Add an entry
     2. Get an entry
     3. Delete an entry
-    4. Exit
+    4. Create master password
+    5. Exit
         """
     )
 
@@ -48,10 +49,34 @@ ADD AN ENTRY:
     elif option == 3:
         delete_entry()
     elif option == 4:
+        if os.path.exists("master.csv"):
+            print("Master password already exists.")
+        else:
+            print(
+                """
+Create a Master Password:
+    1. Generate a master password
+    2. Manually create a master password
+    3. Return
+                """
+            )
+        if option == 1:
+            generate_master()
+        elif option == 2:
+            create_master()
+        elif option == 3:
+            menu()
+
+    elif option == 5:
         close_program()
     else:
         print("Invalid option.")
 
+def main():
+    if os.path.exists("master.csv"):
+        verify_master()
+    else:
+        menu()
 
 if __name__ == "__main__":
     main()
